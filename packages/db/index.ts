8b256df4ff40fff7ca69ad4a52b2dd609e6e0047
Alix47 <khanali18143@gmail.com>
2024-07-18 14:12:25 +0530

    changed the export order in index.ts

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -1,6 +1,11 @@
 import { PrismaClient, Prisma } from '@prisma/client';
 import { type OnRampStatus } from '@prisma/client';
 
+
+export {OnRampStatus};
+export type { Prisma };
+export {PrismaClient};
+
 const prismaClientSingleton = () => {
   return new PrismaClient();
 };
@@ -11,9 +16,6 @@ declare global {
 
 const prisma: ReturnType<typeof prismaClientSingleton> = globalThis.prismaGlobal ?? prismaClientSingleton();
 
-export {OnRampStatus};
-export type { Prisma };
-export {PrismaClient};
 export default prisma;
 
 if (process.env.NODE_ENV !== 'production') {
