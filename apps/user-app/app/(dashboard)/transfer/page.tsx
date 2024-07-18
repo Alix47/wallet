a3b776ffcd1834f45e14ba398fc308b6320741a8
Alix47 <khanali18143@gmail.com>
2024-07-18 13:27:25 +0530

    imported the enum from node modules

--- apps/user-app/app/(dashboard)/transfer/page.tsx
+++ apps/user-app/app/(dashboard)/transfer/page.tsx
@@ -1,4 +1,5 @@
-import prisma, { OnRampStatus } from "@repo/db";
+import prisma from "@repo/db";
+import {OnRampStatus} from ".prisma/client"
 import { AddMoney } from "../../../components/AddMoneyCard";
 import { BalanceCard } from "../../../components/BalanceCard";
 import { OnRampTransactions } from "../../../components/OnRampTransactions";
